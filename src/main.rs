fc5b05cd104984764d71c213a22256944c3c4d08
Bobby Rong <rjl931189261@126.com>
2021-02-27 14:17:12 +0800

    initial driver for VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+
+    // https://os.phil-opp.com/minimal-rust-kernel/#printing-to-screen
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}
