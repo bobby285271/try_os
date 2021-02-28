9adf1b2707140d1d8d6c4f5db59b856f760b9101
Bobby Rong <rjl931189261@126.com>
2021-02-28 13:54:31 +0800

    add safe interior mutability to writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,7 @@
 use core::fmt;
-use volatile::Volatile;
 use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -131,9 +132,9 @@ pub fn print_something() {
 }
 
 lazy_static! {
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
 }
