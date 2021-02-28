c5b83532a82351f2f686c6133ee36a68661e2b85
Bobby Rong <rjl931189261@126.com>
2021-02-28 13:56:02 +0800

    now print directly from _start

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,15 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
 
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
