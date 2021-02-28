48a562f41a80a9247b9d4324a9bd7b9f59c13f5f
Bobby Rong <rjl931189261@126.com>
2021-02-28 15:10:56 +0800

    implement a custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -22,4 +25,13 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+/// A custom test framework for our kernel.
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
