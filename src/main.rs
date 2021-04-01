b873248df2c6605523e8109f95d9656404396fe3
Bobby Rong <rjl931189261@126.com>
2021-04-01 09:11:33 +0800

    insert printing automatically

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,21 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 // our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
@@ -42,10 +57,10 @@ fn panic(info: &PanicInfo) -> ! {
 
 /// A custom test framework for our kernel.
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
@@ -53,9 +68,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 /// cargo test will automatically find and execute all test functions of your crate
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
